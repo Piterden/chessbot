ac4885c7bcff03b31d454f71fff45a37e6790250
Denis Efremov <efremov.a.denis@gmail.com>
2018-04-20 18:25:55 +0300

    Moved handlers.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ const knex = require('knex')
 const Telegraf = require('telegraf')
 const Stage = require('telegraf/stage')
 
-const { debug } = require('./helpers')
 const { gameScene } = require('./scenes')
-const { loadHandler } = require('./handlers')
+const { loadHandler, joinHandler, newHandler } = require('./handlers')
 
 
 const { session } = Telegraf
@@ -39,61 +38,7 @@ bot.use(session())
 bot.use(stage.middleware())
 
 bot.start(...loadHandler())
-
-bot.action(
-  /^new$/,
-  async (ctx) => {
-    const [gameId] = await ctx.db('games')
-      .returning('id')
-      .insert({ user_w: ctx.from.id })
-
-    if (ctx.session.listMessage) {
-      try {
-        await ctx.deleteMessage(ctx.session.listMessage.message_id)
-      }
-      catch (error) {
-        debug(error)
-      }
-      ctx.session.listMessage = null
-    }
-
-    ctx.session.gameId = gameId
-    ctx.scene.enter('game')
-
-    return ctx.answerCbQuery()
-  }
-)
-
-bot.action(
-  /^join\/(\d+)$/,
-  async (ctx) => {
-    const games = await ctx.db('games')
-      .where('id', Number(ctx.match[1]))
-      .select()
-
-    const gameState = games[0]
-
-    if (!gameState.user_b && Number(gameState.user_w) !== ctx.from.id) {
-      await ctx.db('games')
-        .where('id', gameState.id)
-        .update({ user_b: ctx.from.id })
-    }
-
-    if (ctx.session.listMessage) {
-      try {
-        await ctx.deleteMessage(ctx.session.listMessage.message_id)
-      }
-      catch (error) {
-        debug(error)
-      }
-      ctx.session.listMessage = null
-    }
-
-    ctx.session.gameId = gameState.id
-    ctx.scene.enter('game')
-
-    return ctx.answerCbQuery()
-  }
-)
+bot.action(...newHandler())
+bot.action(...joinHandler())
 
 bot.startPolling()
