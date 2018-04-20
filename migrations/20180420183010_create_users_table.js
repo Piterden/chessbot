6ebf8cc7ac82fdc93889b9e497a0df0a98347aaf
Denis Efremov <efremov.a.denis@gmail.com>
2018-04-20 18:35:13 +0300

    Added users table migration.

---
+++ migrations/20180420183010_create_users_table.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+
+exports.up = async (knex, Promise) => await knex.schema.hasTable('users')
+  ? null
+  : knex.schema.createTable('users', (table) => {
+    table.integer('id')
+    table.boolean('is_bot')
+    table.string('first_name')
+    table.string('last_name')
+    table.string('username')
+    table.string('language_code')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+
+    table.primary('id')
+  })
+
+exports.down = async (knex, Promise) => await knex.schema.hasTable('users')
+  ? knex.schema.dropTable('users')
+  : null
