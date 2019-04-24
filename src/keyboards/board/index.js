e09ab093bde7fa9459f0a24e80aa013068c7fc01
Piterden <efremov.a.denis@gmail.com>
2019-04-24 11:46:29 +0300

    Fixed join button placement

--- src/keyboards/board/index.js
+++ src/keyboards/board/index.js
@@ -6,34 +6,33 @@ const { Markup } = Telegraf
 
 module.exports = (board, isWhite, actions) => {
   const horizontal = 'abcdefgh'.split('')
-  const vertical = Array.form({ length: 8 }).map((item, idx) => idx + 1).reverse()
+  const vertical = Array.from({ length: 8 }).map((item, idx) => idx + 1).reverse()
 
-  const boardMarkup = [
-    ...vertical.map((row) => horizontal.map((col) => {
-      const square = board
-        .find(({ file, rank }) => file === col && rank === row)
+  const boardMarkup = vertical.map((row) => horizontal.map((col) => {
+    const square = board
+      .find(({ file, rank }) => file === col && rank === row)
 
-      if (square && square.piece) {
-        return square.destination
-          ? {
-            text: `X${emodji[square.piece.side.name][square.piece.type]}`,
-            callback_data: `${col}${row}`,
-          }
-          : {
-            text: `${emodji[square.piece.side.name][square.piece.type]}`,
-            callback_data: `${col}${row}`,
-          }
-      }
-
+    if (square && square.piece) {
       return square.destination
-        ? { text: 'O', callback_data: `${col}${row}` }
-        : { text: unescape('%u0020'), callback_data: `${col}${row}` }
-    })),
-    actions,
-  ]
+        ? {
+          text: `X${emodji[square.piece.side.name][square.piece.type]}`,
+          callback_data: `${col}${row}`,
+        }
+        : {
+          text: `${emodji[square.piece.side.name][square.piece.type]}`,
+          callback_data: `${col}${row}`,
+        }
+    }
 
-  return Markup.inlineKeyboard(isWhite
-    ? boardMarkup
-    : boardMarkup.map((row) => row.reverse()).reverse()
-  ).extra()
+    return square.destination
+      ? { text: 'O', callback_data: `${col}${row}` }
+      : { text: unescape('%u0020'), callback_data: `${col}${row}` }
+  }))
+
+  return Markup.inlineKeyboard([
+    ...(isWhite
+      ? boardMarkup
+      : boardMarkup.map((row) => row.reverse()).reverse()),
+    actions,
+  ]).extra()
 }
