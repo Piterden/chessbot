dc07d8f8c404f5eb999d2077e724cadfb5233b6d
Piterden <efremov.a.denis@gmail.com>
2019-04-24 09:26:32 +0300

    Added test button

--- src/handlers/inline/index.js
+++ src/handlers/inline/index.js
@@ -1,9 +1,12 @@
+const { debug } = require('@/helpers')
+const { board } = require('@/keyboards')
+
 const img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Starting_position_in_a_chess_game.jpg/1024px-Starting_position_in_a_chess_game.jpg'
 
 module.exports = () => [
   'inline_query',
   async (ctx) => {
-    console.log(ctx.from)
+    debug(ctx.from)
     await ctx.answerInlineQuery([
       {
         id: 1,
@@ -12,7 +15,15 @@ module.exports = () => [
         title: 'Play',
         input_message_content: {
           message_text: 'hi',
-        } },
+        },
+        reply_markup: {
+          inline_keyboard: [
+            [
+              { text: 'Hi', callback_data: 'hi' },
+            ],
+          ],
+        },
+      },
     ])
   },
 ]
