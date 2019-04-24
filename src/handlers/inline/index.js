cc4d91c6f8c13f4719bd9c2221870d689d50fdec
Piterden <efremov.a.denis@gmail.com>
2019-04-24 11:24:48 +0300

    Trying to show board

--- src/handlers/inline/index.js
+++ src/handlers/inline/index.js
@@ -1,13 +1,10 @@
-const { debug } = require('@/helpers')
-// const { board } = require('@/keyboards')
+const chess = require('chess')
 
-const markup = {
-  inline_keyboard: [
-    [
-      { text: 'Hi', callback_data: 'hi' },
-    ],
-  ],
-}
+const { debug } = require('@/helpers')
+const { board } = require('@/keyboards')
+
+const gameClient = chess.create({ PGN: true })
+const status = gameClient.getStatus()
 
 module.exports = () => [
   'inline_query',
@@ -21,9 +18,10 @@ module.exports = () => [
         photo_url: 'https://crossword.live/img/w.png',
         title: 'Play with white pieces',
         input_message_content: {
-          message_text: `${ctx.update.inline_query.from.first_name} is white side.`,
+          message_text: `Black (top): ?
+White (bottom): ${ctx.update.inline_query.from.first_name}`,
         },
-        reply_markup: markup,
+        reply_markup: board(status.board.squares, true),
       },
       {
         id: 2,
@@ -32,13 +30,14 @@ module.exports = () => [
         photo_url: 'https://crossword.live/img/b.png',
         title: 'Play with black pieces',
         input_message_content: {
-          message_text: `${ctx.update.inline_query.from.first_name} is black side.`,
+          message_text: `White (top): ?
+Black (bottom): ${ctx.update.inline_query.from.first_name}`,
         },
-        reply_markup: markup,
+        reply_markup: board(status.board.squares, false),
       },
     ], {
       is_personal: true,
-      cache_time: 0,      
+      cache_time: 0,
     })
   },
 ]
