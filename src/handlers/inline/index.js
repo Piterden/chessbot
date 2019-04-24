66a6cf52153a98a313ae3939510a70b7d1f07817
Piterden <efremov.a.denis@gmail.com>
2019-04-24 13:33:21 +0300

    Added user saving

--- src/handlers/inline/index.js
+++ src/handlers/inline/index.js
@@ -6,10 +6,41 @@ const { board } = require('@/keyboards')
 const gameClient = chess.create({ PGN: true })
 const status = gameClient.getStatus()
 
+const escapeUser = (user) => Object.keys(user).reduce((acc, key) => {
+  acc[key] = typeof user[key] === 'string' ? escape(user[key]) : user[key]
+  return acc
+}, {})
+
+const unescapeUser = (user) => Object.keys(user).reduce((acc, key) => {
+  acc[key] = typeof user[key] === 'string' ? unescape(user[key]) : user[key]
+  return acc
+}, {})
+
 module.exports = () => [
   'inline_query',
   async (ctx) => {
     debug(ctx.update)
+
+    let user = await ctx.db('users')
+      .where('id', ctx.update.inline_query.from.id)
+      .first()
+
+    if (user) {
+      user = unescapeUser(user)
+
+      if (JSON.stringify(user) !== JSON.stringify(ctx.update.inline_query.from)) {
+        await ctx.db('users')
+          .where('id', user.id)
+          .update(escapeUser(ctx.update.inline_query.from))
+      }
+    } else {
+      const users = await ctx.db('users')
+        .insert(escapeUser(ctx.update.inline_query.from))
+        .returning(Object.keys(ctx.update.inline_query.from))
+
+      user = unescapeUser(users[0])
+    }
+
     await ctx.answerInlineQuery([
       {
         id: 1,
@@ -23,7 +54,7 @@ White (bottom): ${ctx.update.inline_query.from.first_name}`,
         },
         ...board(status.board.squares, true, [{
           text: 'Join the game',
-          callback_data: `join::${ctx.update.inline_query.from.id}`,
+          callback_data: `join::w::${user.id}`,
         }]),
       },
       {
@@ -38,7 +69,7 @@ Black (bottom): ${ctx.update.inline_query.from.first_name}`,
         },
         ...board(status.board.squares, false, [{
           text: 'Join the game',
-          callback_data: `join::${ctx.update.inline_query.from.id}`,
+          callback_data: `join::b::${user.id}`,
         }]),
       },
     ], {
