42184c9364a374b4f4afad4aa2cd3d3fbaa357a3
Piterden <efremov.a.denis@gmail.com>
2019-04-27 02:03:56 +0300

    Added dialect

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,9 @@ const Sequelize = require('sequelize')
 
 const { DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env
 
-const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD).sync()
+const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
+  dialect: 'postgres',
+}).sync()
 
 module.exports = {
   sequelize,
