ead5643f6bb7a22f227d3e508b37569f36182359
@kolayv <kolay.va@gmail.com>
2019-04-27 15:33:44 +0000

    empty user model

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,15 @@
 const Sequelize = require('sequelize')
-
+const userModel = require('./user-model')
 const { DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env
 
 const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
   dialect: 'postgres',
-}).sync()
+})
+
+const user = sequelize.define(...userModel)
+
+sequelize.sync()
 
 module.exports = {
-  sequelize,
+  sequelize, user,
 }
