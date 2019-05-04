2cb0cc1b7bb8b74b007700d11293ed430b8afbec
Piterden <efremov.a.denis@gmail.com>
2019-05-04 05:31:01 +0300

    Added rotation settings

--- migrations/20180410063744_create_games_table.js
+++ migrations/20180410063744_create_games_table.js
@@ -9,6 +9,7 @@ exports.up = async (knex, Promise) => (await knex.schema.hasTable('games'))
     table.string('inline_id').notNullable().unique().index()
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at').defaultTo(knex.fn.now())
+    table.text('config').defaultTo('{}')
 
     table.foreign('whites_id').references('id').on('users')
     table.foreign('blacks_id').references('id').on('users')
--- src/handlers/inlineSettings/index.js
+++ src/handlers/inlineSettings/index.js
@@ -1,24 +1,66 @@
 const { debug } = require('@/helpers')
 
 module.exports = () => [
-  /^settings(?:::(\w+))?$/,
+  /^settings(?:::(\w+))?(?:::(\w+))?$/,
   async (ctx) => {
-    await ctx.editMessageReplyMarkup({
-      inline_keyboard: [
-        [
-          { text: 'Board Rotation', callback_data: 'settings::rotation' },
-          { text: '2', callback_data: 'settings::2' },
-        ],
-        [
-          { text: '3', callback_data: 'settings::3' },
-          { text: '4', callback_data: 'settings::4' },
-        ],
-        [
-          { text: '⬅️ Back to game', callback_data: 'back' },
-        ],
-      ],
-    }).catch(debug)
+    switch (ctx.match[1]) {
+      case 'rotation':
+        switch (ctx.match[2]) {
+          case 'whites':
+          case 'blacks':
+          case 'dynamic':
+            const game = await ctx.db('games')
+              .where('inline_id', ctx.callbackQuery.inline_message_id)
+              .first()
+              .catch(debug)
 
-    return ctx.answerCbQuery('Please choose a setting you want to change!')
+            if (game) {
+              let { config } = game
+
+              config = JSON.parse(config) || {}
+              config.rotation = ctx.match[2]
+
+              await ctx.db('games')
+                .update({ config: JSON.stringify(config) })
+                .where('id', game.id)
+                .catch(debug)
+            }
+
+            break
+
+          default:
+            await ctx.editMessageReplyMarkup({
+              inline_keyboard: [
+                [
+                  { text: 'Whites at bottom', callback_data: 'settings::rotation::whites' },
+                ],
+                [
+                  { text: 'Blacks at bottom', callback_data: 'settings::rotation::blacks' },
+                ],
+                [
+                  { text: 'Current mover at bottom', callback_data: 'settings::rotation::dynamic' },
+                ],
+                [
+                  { text: '⬅️ Back to settings', callback_data: 'settings' },
+                ],
+              ],
+            }).catch(debug)
+        }
+        break
+
+      default:
+        await ctx.editMessageReplyMarkup({
+          inline_keyboard: [
+            [
+              { text: 'Board Rotation', callback_data: 'settings::rotation' },
+            ],
+            [
+              { text: '⬅️ Back to game', callback_data: 'back' },
+            ],
+          ],
+        }).catch(debug)
+
+        return ctx.answerCbQuery('Please choose a setting you want to change!')
+    }
   },
 ]
