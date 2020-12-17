87018fea639020ef36c037a5072f5eb5be11313e
Piterden <efremov.a.denis@gmail.com>
2020-12-17 15:49:45 +0300

    Added board show on select old game.

--- src/handlers/inlineQuery/index.js
+++ src/handlers/inlineQuery/index.js
@@ -1,7 +1,13 @@
 const chess = require('chess')
 
-const { board } = require('@/keyboards')
+const { board, actions } = require('@/keyboards')
 const { debug, isWhiteTurn } = require('@/helpers')
+
+const statusMessage = ({
+  isCheck,
+  isCheckmate,
+  isRepetition,
+}) => `${isCheck ? '|CHECK|' : ''}${isCheckmate ? '|CHECKMATE|' : ''}${isRepetition ? '|REPETITION|' : ''}`
 
 module.exports = () => async (ctx) => {
   let user = await ctx.db('users')
@@ -19,76 +25,6 @@ module.exports = () => async (ctx) => {
     await ctx.db('users').insert(ctx.from).catch(debug)
     user = await ctx.db('users').where('id', ctx.from.id).first().catch(debug)
   }
-
-  // const match = ctx.inlineQuery.query.match(/^\s*(\d+)::(\d+)\s*$/)
-
-  // if (match) {
-  //   const games = await ctx.db('games')
-  //     .where({
-  //       whites_id: match[1],
-  //       blacks_id: match[2],
-  //     })
-  //     .orWhere({
-  //       whites_id: match[2],
-  //       blacks_id: match[1],
-  //     })
-  //     .catch(debug)
-
-  //   if (games && games.length > 0) {
-  //     const enemyId = Number(match[1]) === ctx.from.id
-  //       ? Number(match[2])
-  //       : Number(match[1])
-
-  //     const list = await Promise.all(games.slice(0, 50).map(async (game, idx) => {
-  //       const enemy = await ctx.db('users')
-  //         .where('id', enemyId)
-  //         .first()
-  //         .catch(debug)
-
-  //       const moves = await ctx.db('moves')
-  //         .where('game_id', game.id)
-  //         .orderBy('created_at', 'asc')
-  //         .select()
-  //         .catch(debug)
-
-  //       moves.forEach(({ entry }) => {
-  //         try {
-  //           gameClient.move(entry)
-  //         } catch (error) {
-  //           debug(error)
-  //         }
-  //       })
-
-  //       status = gameClient.getStatus()
-
-  //       return {
-  //         id: idx + 1,
-  //         type: 'article',
-  //         title: `Game #${game.id}`,
-  //         description: `Moves: ${moves.length}.
-  // ${isWhiteTurn(moves) ? 'Whites' : 'Blacks'} turn.`,
-  //         input_message_content: {
-  //           message_text: `Black (top): ${enemy.first_name}
-  // White (bottom): ${user.first_name}`,
-  //         },
-  //         ...board(
-  //           status.board.squares,
-  //           isWhiteTurn(moves),
-  //           [{
-  //             text: 'Settings',
-  //             callback_data: 'settings',
-  //           }],
-  //           `::${game.id}`
-  //         ),
-  //       }
-  //     }))
-
-  //     await ctx.answerInlineQuery(list, {
-  //       is_personal: true,
-  //       cache_time: 0,
-  //     })
-  //   }
-  // }
 
   const games = await ctx.db('games')
     .where({ whites_id: ctx.from.id })
@@ -128,7 +64,7 @@ module.exports = () => async (ctx) => {
 
   const list = await Promise.all(games.slice(0, 48).map(async (game, idx) => {
     const gameClient = chess.create({ PGN: true })
-    // let status
+    let status
 
     const enemy = await ctx.db('users')
       .where('id', game.whites_id === ctx.from.id ? game.blacks_id : game.whites_id)
@@ -148,16 +84,15 @@ module.exports = () => async (ctx) => {
       }
     })
 
-    // status = gameClient.getStatus()
+    status = gameClient.getStatus()
     const fen = getFen(gameClient.game.board)
-    console.log(fen)
 
     return {
       id: idx + 3,
       type: 'article',
       title: `You vs ${enemy.first_name}`,
-      description: `Started ${game.created_at.getDate()}.${game.created_at.getMonth()}.${game.created_at.getFullYear()} | Moves ${moves.length}
-${isWhiteTurn(moves) ? 'Whites' : 'Blacks'} turn.`,
+      description: `Started ${game.created_at.getDate()}.${game.created_at.getMonth()}.${game.created_at.getFullYear()} | ${moves.length} turns
+${statusMessage(status)}`,
       thumb_url: `https://chessboardimage.com/${fen.replace(/\//g, '')}.png`,
       thumb_width: 418,
       thumb_height: 418,
@@ -166,18 +101,11 @@ ${isWhiteTurn(moves) ? 'Whites' : 'Blacks'} turn.`,
 Black (top): ${enemy.first_name}
 White (bottom): ${user.first_name}`,
       },
-      // ...board({
-      //   board: status.board.squares,
-      //   isWhite: isWhiteTurn(moves),
-      //   callbackOverride: `join::w::${user.id}`,
-      //   actions: [{
-      //     text: 'Join the game',
-      //     callback_data: `join::w::${user.id}`,
-      //   }, {
-      //     text: 'New game',
-      //     switch_inline_query_current_chat: '',
-      //   }],
-      // }),
+      ...board({
+        board: status.board.squares,
+        isWhite: isWhiteTurn(moves),
+        actions: actions(),
+      }),
     }
   }))
 
@@ -189,7 +117,6 @@ White (bottom): ${user.first_name}`,
       id: 1,
       type: 'sticker',
       sticker_file_id: 'CAADAgADNAADX5T2DgeepFdKYLnKAg',
-      // title: 'Start a new game as white',
       input_message_content: {
         message_text: `Black (top): ?
 White (bottom): ${user.first_name}
@@ -212,7 +139,6 @@ Waiting for a black side`,
       id: 2,
       type: 'sticker',
       sticker_file_id: 'CAADAgADMwADX5T2DqhR9w5HSpCZAg',
-      // title: 'Start a new game as black',
       input_message_content: {
         message_text: `White (top): ?
 Black (bottom): ${user.first_name}
