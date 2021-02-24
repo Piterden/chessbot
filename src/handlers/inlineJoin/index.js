06fc60e3e0179bc397684d1a170a15e58ef9be56
Piterden <efremov.a.denis@gmail.com>
2021-02-24 03:14:07 +0300

    Added whites and blacks abstraction

--- src/handlers/inlineJoin/index.js
+++ src/handlers/inlineJoin/index.js
@@ -27,13 +27,17 @@ module.exports = () => [
     }
 
     const enemy = await ctx.db('users').where('id', enemyId).first().catch(debug)
+    const whites = iAmWhite ? user : enemy
+    const blacks = iAmWhite ? enemy : user
 
     await ctx.db('games').insert({
-      whites_id: iAmWhite ? ctx.from.id : enemy.id,
-      blacks_id: iAmWhite ? enemy.id : ctx.from.id,
+      whites_id: whites.id,
+      blacks_id: blacks.id,
       inline_id: ctx.callbackQuery.inline_message_id,
       config: JSON.stringify({ rotation: 'dynamic' }),
     }).catch(debug)
+
+    ctx.game.joined = true
 
     const game = await ctx.db('games')
       .where('inline_id', ctx.callbackQuery.inline_message_id)
@@ -50,32 +54,24 @@ module.exports = () => [
     const gameClient = chess.create({ PGN: true })
     const status = gameClient.getStatus()
 
-    ctx.game.lastBoard = board({
-      board: status.board.squares,
-      isWhite: true,
-      actions: actions(),
-    })
-
     log(
       preLog('JOIN', `${game.id} ${makeUserLog(enemy)} ${makeUserLog(user)}`),
       ctx,
     )
 
     await ctx.editMessageCaption(
-      iAmWhite
-        ? `Black  (top): [${enemy.first_name}](tg://user?id=${enemy.id})
-White  (bottom): [${user.first_name}](tg://user?id=${user.id})
-White's turn | [Discussion](https://t.me/chessy_bot_chat)`
-        : `Black  (top): [${user.first_name}](tg://user?id=${user.id})
-White  (bottom): [${enemy.first_name}](tg://user?id=${enemy.id})
+      `Black  (top): [${blacks.first_name}](tg://user?id=${blacks.id})
+White  (bottom): [${whites.first_name}](tg://user?id=${whites.id})
 White's turn | [Discussion](https://t.me/chessy_bot_chat)`,
       {
-        ...ctx.game.lastBoard,
+        ...board({
+          board: status.board.squares,
+          isWhite: true,
+          actions: actions(),
+        }),
         parse_mode: 'Markdown',
       },
     ).catch(debug)
-
-    ctx.game.joined = true
 
     return ctx.answerCbQuery('Now play!').catch(debug)
   },
