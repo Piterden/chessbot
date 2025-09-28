813e742b32b0f9fdc90739456a1acb905bc9bc76
Piterden <efremov.a.denis@gmail.com>
2025-09-28 06:02:39 +0300

    Try

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ bot.context.db = knex(knexConfig)
 bot.use(session({
   property: 'game',
   getSessionKey: (ctx) => ctx.callbackQuery?.inline_message_id ||
-    `${ctx.from?.id}:${ctx.chat?.id}:${ctx.message?.message_id || ctx.callbackQuery?.message_id}`,
+    `${ctx.chat?.id}:${ctx.callbackQuery?.message_id || ctx.message?.message_id + 1}`,
 }))
 
 // bot.command('start', startHandler())
